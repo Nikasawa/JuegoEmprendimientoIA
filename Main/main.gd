a542beb28a9f6f70b3ac21ce06666ea7b8fdbf80
Nikasawa <121456466+Nikasawa@users.noreply.github.com>
2024-05-24 01:54:41 -0300

    MobRotaJugador

--- Main/main.gd
+++ Main/main.gd
@@ -10,7 +10,7 @@ var NewPoint2 = 0
 var angulo = 0
 var spawn = 1
 
-
+var grupo = []
 
 signal value_changed(Nuevo_valor)
 
@@ -22,14 +22,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	GlobalSignal.emit_signal('Nuevo_valor', $Jugador.position)
-	
-	$Line2D.clear_points()
-	$Line2D.add_point(Vector2(NewPoint1, NewPoint2))
-	$Line2D.add_point(Vector2($Jugador.position[0], $Jugador.position[1])) 
-	$Line2D.add_point(Vector2($Jugador.position[0], NewPoint2)) # Diferencia en Y de la posicion entre el jugador y el mob
-	$Line2D.add_point(Vector2(NewPoint1, NewPoint2)) # Diferencia en X de la posicion entre el jugador y el mob
+
+	# Se almacenan todas las entidades (solo enemigos por ahora), en el que se revisa uno por uno ejecutando funciones
+	# Espero que exista una forma mas optima de pasar por cada uno de los bichos si se supone que el juego esta pensado para tener un monton de entidades a la vez	
+	for entidades in grupo:
+		entidades.vista($Jugador.position[0], $Jugador.position[1])
 	
 	if Input.is_action_pressed("Saltar"):
 		for x in range($Line2D.get_point_count()):
@@ -57,10 +54,14 @@ func mobSpawn(delta):
 	# var mob_spawn_location = $MobPath/MobSpawnLocation # Toma el path creado en la escena Main como posibles posiciones para spawnear
 	
 	# Set the mob's position to a random location.
-	mob.position = Vector2(350, 550) # mob_spawn_location.position
+	mob.position = Vector2(350, 150) # mob_spawn_location.position
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+	
+	grupo.append(mob)
+	
+	# mob.vista()
 	
 	NewPoint1 = mob.position[0]
 	NewPoint2 = mob.position[1]
--- Scripts/Mob.gd
+++ Scripts/Mob.gd
@@ -14,3 +14,22 @@ func _process(delta):
 	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+	
+func vista(ejeX, ejeY):
+	
+	rotation = 0
+	
+	var diferenciaX = ejeX - position[0]
+	var diferenciaY = ejeY - position[1]
+	
+	var hipotenusa = sqrt(diferenciaX ** 2 + diferenciaY ** 2)
+	
+	var angulo = asin(diferenciaX / hipotenusa) 
+	
+	const VUELTA_COMPLETA = 180 * (PI / 180)
+	
+	if diferenciaY >= 0:
+		rotation += VUELTA_COMPLETA
+		angulo = asin(diferenciaX / hipotenusa) * -1
+		
+	rotation += angulo
